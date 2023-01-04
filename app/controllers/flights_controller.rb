784d05ae06d3d9829286fdfe003c577759e99eae
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-05 00:19:13 +0100

    search without date

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,30 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @flights = []
   end
-end
 
- private
+  def search
+    # origin = params[:search][:origin]
+    # destination = params[:search][:destination]
+    # date = params[:search][:date]
+    # @flights = Flight.joins(:departure_airport).where(departure_airport: origin)
 
-def group_dates_by_month(dates)
-  dates.group_by { |d| d.strftime("%B %Y") }.map { |month_year, month_dates| [month_year, month_dates.map { |d| d.strftime("%Y-%m") }] }
+    origin_id = params[:search][:origin]
+    destination_id = params[:search][:destination]
+    @flights = Flight.where(departure_airport_id: origin_id, arrival_airport_id: destination_id)
+
+    if @flights.empty?
+      flash.now[:danger] = "No flights found"
+    else
+      flash.now[:success] = "Flights found"
+    end
+
+    render :index
+  end
+
+  private
+
+  def group_dates_by_month(dates)
+    dates.group_by { |d| d.strftime("%B %Y") }.map { |month_year, month_dates| [month_year, month_dates.map { |d| d.strftime("%Y-%m") }] }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get "flights/index"
+  get "flights/search", to: "flights#search", as: "search"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+
   root "flights#index"
 end
