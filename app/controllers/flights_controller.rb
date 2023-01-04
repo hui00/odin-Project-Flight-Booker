2e62f285b3586803e1b6d801facbd9e4aa2922c3
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-04 02:19:15 +0100

    dropdown for the flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,11 @@
 class FlightsController < ApplicationController
   def index
+    @flights = Flight.all
   end
+end
+
+ private
+
+def group_dates_by_month(dates)
+  dates.group_by { |d| d.strftime("%B %Y") }.map { |month_year, month_dates| [month_year, month_dates.map { |d| d.strftime("%Y-%m") }] }
 end
