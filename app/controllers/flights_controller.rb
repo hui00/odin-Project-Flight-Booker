2caa2f2d41893494775e0f772e496814ce043a1d
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-05 21:54:03 +0100

    search by date optional

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,14 +4,15 @@ class FlightsController < ApplicationController
   end
 
   def search
-    # origin = params[:search][:origin]
-    # destination = params[:search][:destination]
-    # date = params[:search][:date]
-    # @flights = Flight.joins(:departure_airport).where(departure_airport: origin)
-
     origin_id = params[:search][:origin]
     destination_id = params[:search][:destination]
     @flights = Flight.where(departure_airport_id: origin_id, arrival_airport_id: destination_id)
+
+    month = params[:search][:month]
+    if month.present?
+      month_number = Date::MONTHNAMES.index(month)
+      @flights = @flights.where("EXTRACT(MONTH FROM start_datetime) = ?", month_number)
+    end
 
     if @flights.empty?
       flash.now[:danger] = "No flights found"
@@ -20,11 +21,5 @@ class FlightsController < ApplicationController
     end
 
     render :index
-  end
-
-  private
-
-  def group_dates_by_month(dates)
-    dates.group_by { |d| d.strftime("%B %Y") }.map { |month_year, month_dates| [month_year, month_dates.map { |d| d.strftime("%Y-%m") }] }
   end
 end
