269a4339fab6dbc92000bdf0675c15dc252dc6c4
hui00 <kevinbrinkschulte@hotmail.de>
2023-01-11 01:25:30 +0100

    add associations to flight model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "flights/index"
   get "flights/search", to: "flights#search", as: "search"
   get "flights/new", to: "flights#new", as: "new"
+  get "bookings/new", to: "bookings#new", as: "booking"
+  post "bookings/new", to: "bookings#create", as: "create"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
